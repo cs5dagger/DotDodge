b7e3db0ce93aca02f5f89a8180d03c6d6722eac0
Dagger <cssameer99@gmail.com>
2025-02-01 23:09:48 +0530

    add code to update score. Add level speeds and max levels. UPdate code in gameplay manager script

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool HasGameFinished;
+
+    [SerializeField] private TMP_Text ScoreText;
+
+    private float Score;
+    private float ScoreSpeed;
+    private int currentLevel;
+
+    [SerializeField] private List<int> LevelSpeed, LevelMax;
+
+    private void Awake()
     {
-        
+        GameManager.Instance.IsInitialized = true;
+        HasGameFinished = false;
+        Score = 0;
+        currentLevel = 0;
+        ScoreText.text = ((int)Score).ToString();
+        ScoreSpeed = LevelSpeed[currentLevel];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (HasGameFinished) return;
+        Score += ScoreSpeed * Time.deltaTime;
+        ScoreText.text = ((int)Score).ToString();
+
+        if(Score > LevelMax[Mathf.Clamp(currentLevel, 0, LevelSpeed.Count - 1)])
+        {
+            currentLevel = Mathf.Clamp(currentLevel + 1, 0, LevelSpeed.Count - 1);
+            ScoreSpeed = LevelSpeed[currentLevel];
+        }
+    }
+
+    public void GameOver()
+    {
+        HasGameFinished = true;
+        GameManager.Instance.CurrentScore = (int)Score;
+        StartCoroutine(GameOverCoroutine());
+    }
+
+    private IEnumerator GameOverCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        GameManager.Instance.GoToMainMenu();
     }
 }
