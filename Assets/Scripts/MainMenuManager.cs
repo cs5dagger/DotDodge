bf4a9b87c0b553afaeaa2f9a5226d991f13ae81d
Dagger <cssameer99@gmail.com>
2025-02-01 22:15:33 +0530

    Setup main menu manager scene and set up coroutine to show score and animation curve

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,86 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private TMP_Text ScoreText;
+    
+    [SerializeField]
+    private TMP_Text NewBestText;
+    
+    [SerializeField]
+    private TMP_Text HighScoreText;
+
+    private void Awake()
     {
-        
+        if(GameManager.Instance.IsInitialized)
+        {
+            StartCoroutine(ShowScore());
+        }
+        else
+        {
+            ScoreText.gameObject.SetActive(false);
+            NewBestText.gameObject.SetActive(false); 
+            HighScoreText.text = GameManager.Instance.HighScore.ToString();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float AnimationTime;
+    [SerializeField] private AnimationCurve SpeedCurve;
+
+    /// <summary>
+    /// Display and update score
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ShowScore()
     {
-        
+        int tempScore = 0;
+        ScoreText.text = tempScore.ToString();
+
+        int currScore = GameManager.Instance.CurrentScore;
+        int highScore = GameManager.Instance.HighScore;
+
+        if(highScore < currScore)
+        {
+            NewBestText.gameObject.SetActive(true);
+            GameManager.Instance.HighScore = highScore;
+        }
+        else
+        {
+            NewBestText.gameObject.SetActive(false);
+        }
+
+        HighScoreText.text = GameManager.Instance.HighScore.ToString();
+        float speed = 1 / AnimationTime;
+        float timeElapsed = 0f;
+
+        /// Play score animation
+        while(timeElapsed < 1f)
+        {
+            timeElapsed += speed * Time.deltaTime;
+
+            tempScore = (int)(SpeedCurve.Evaluate(timeElapsed) * currScore);
+            ScoreText.text = tempScore.ToString();
+            yield return null;
+        }
+
+        tempScore = currScore;
+        ScoreText.text = tempScore.ToString();
     }
+
+    [SerializeField] private AudioClip ClickClip;
+
+    /// <summary>
+    /// Play sound and change scene to play
+    /// </summary>
+    public void ClickedPlay()
+    {
+        SoundManager.instance.PlaySound(ClickClip);
+        GameManager.Instance.GoToGamePlay();
+    }
+
+
 }
