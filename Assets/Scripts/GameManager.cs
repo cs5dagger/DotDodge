0228a8774d95403964873f9e1d1fc2f0dcb057c9
Dagger <cssameer99@gmail.com>
2025-02-01 21:52:02 +0530

    Setup game manager script, also set script execution order in the project settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Init();
+            return;
+        }    
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsInitialized { get; set; }
+    public int CurrentScore { get; set; }
+
+    private string highScoreKey = "HighScore";
+
+    public int HighScore
+    { 
+        get { return PlayerPrefs.GetInt(highScoreKey, 0); }
+        set 
+        { 
+            PlayerPrefs.SetInt(highScoreKey, value);
+            PlayerPrefs.Save(); /// Ensure data is saved
+        }
+    }
+
+    /// <summary>
+    /// Setup variables and data
+    /// </summary>
+    private void Init()
     {
-        
+        CurrentScore = 0;
+        IsInitialized = false;
+    }
+
+    private const string MainMenu = "MainMenu";
+    private const string GamePlay = "GamePlay";
+
+    public void GoToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(MainMenu);
+    }
+    
+    public void GoToGamePlay()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GamePlay);
     }
 }
