f8d9c321a1a9b760652618f1fe19a9b92206420c
Dagger <cssameer99@gmail.com>
2025-02-01 21:39:10 +0530

    Setup sound manager script and disable play on awake in audio source

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager instance;
+
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            /// Sound manager is not setup
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        else
+        {
+            /// Sound manager is already setup
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private AudioSource EffectSource;
+
+    public void PlaySound(AudioClip clip)
     {
-        
+        EffectSource.PlayOneShot(clip);
     }
+
 }
